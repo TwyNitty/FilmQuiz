57432b0ed229c14157d400218f934baa7d2b0c30
11179544@college.wlc.ac.uk <11179544@college.wlc.ac.uk>
2023-11-14 12:10:57 +0000

    Created high score

--- FilmQuiz/frmHighScore.Designer.vb
+++ FilmQuiz/frmHighScore.Designer.vb
@@ -22,9 +22,66 @@ Partial Class frmHighScore
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.lstHighscore = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblMessage
+        '
+        Me.lblMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(55, 36)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(733, 75)
+        Me.lblMessage.TabIndex = 0
+        Me.lblMessage.Text = "Congratulations Your Score Will Be Added To The Hall Of Fame"
+        '
+        'lstHighscore
+        '
+        Me.lstHighscore.FormattingEnabled = True
+        Me.lstHighscore.Location = New System.Drawing.Point(142, 159)
+        Me.lstHighscore.Name = "lstHighscore"
+        Me.lstHighscore.Size = New System.Drawing.Size(387, 147)
+        Me.lstHighscore.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(604, 321)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(154, 54)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Again"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(604, 381)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(153, 47)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Quit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'frmHighScore
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstHighscore)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Name = "frmHighScore"
         Me.Text = "frmHighScore"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents lstHighscore As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- FilmQuiz/frmHighScore.vb
+++ FilmQuiz/frmHighScore.vb
@@ -1,3 +1,16 @@
 ﻿Public Class frmHighScore
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        frmMain.Show()
+        Me.Hide()
+    End Sub
+    Public Sub doScore()
+        If playerScore < 1 Then
+            lblMessage.Text = "Sorry Your Score Was Not Good Enough"
+        Else
+            lblMessage.Text = "Congratulations Your Score Will Be Added To The Hall Of Fame"
+            lstHighscore.Items.Add(PlayerName & vbTab & playerScore)
+        End If
+    End Sub
+    End If
+    End Sub
 End Class
